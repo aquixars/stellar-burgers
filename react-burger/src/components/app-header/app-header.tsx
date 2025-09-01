8558528ddf7c4bad9e8577742a42a2170af489c1
Valery Kartashov <vkartashov@sigma-it.ru>
2025-09-01 16:08:45 +0300

    Update app-header.tsx

--- react-burger/src/components/app-header/app-header.tsx
+++ react-burger/src/components/app-header/app-header.tsx
@@ -1,10 +1,26 @@
-import { BurgerIcon, ListIcon, Logo, LogoutIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components"
+import styles from './app-header.module.css';
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 export const AppHeader = () => {
-    return <div>
-        <BurgerIcon type={"primary"}/> Конструктор
-        <ListIcon type={"secondary"}/> Лента заказов
-        <Logo />
-        <ProfileIcon type={"secondary"} />Личный кабинет
-    </div>
-}
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.first_third_part}>
+          <BurgerIcon type={'primary'} />
+            <p className='text text_type_main-default ml-2 mr-10'>
+                Конструктор
+            </p>
+          <ListIcon type={'primary'} />
+            <p className='text text_type_main-default ml-2'>Лента заказов</p>
+        </div>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <div className={styles.profile}>
+          <ProfileIcon type={'primary'} />
+          <p className='text text_type_main-default ml-2'>Личный кабинет</p>
+        </div>
+      </nav>
+    </header>
+  );
+};
