99d02964941b77c1ba7b03da2533b520147b5f82
Valery Kartashov <vkartashov@sigma-it.ru>
2025-11-18 20:08:23 +0300

    Updates

--- react-burger/src/components/burger-ingredients/burger-ingredients.utils.ts
+++
@@ -1,8 +0,0 @@
-import { IngredientType, TIngredient } from "../app/app.typed";
-
-export const sortIngredientsByType = (data: TIngredient[]) => {
-    const buns = data.filter((i) => i.type === IngredientType.BUN);
-    const mains = data.filter((i) => i.type === IngredientType.MAIN);
-    const sauces = data.filter((i) => i.type === IngredientType.SAUCE);
-    return { buns, mains, sauces };
-};
--- react-burger/src/components/form/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hint } from "./components/hint";
-import { InputContainer } from "./components/input-container";
-import { Submit } from "./components/submit";
-import { Title } from "./components/title";
-import Form from "./form";
-
-export default Form;
-export { Hint, InputContainer, Submit, Title };
