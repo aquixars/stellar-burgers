249fd3dce6a0b81647fbc22fa969e8dca7a91e67
Valery Kartashov <vkartashov@sigma-it.ru>
2025-11-20 01:21:10 +0300

    review

--- react-burger/cypress/integration/app/constructor-page.spec.ts
+++ react-burger/cypress/integration/app/constructor-page.spec.ts
@@ -1,5 +1,13 @@
 import { API_URL } from "../../../src/utils/api";
 
+const SELECTORS = {
+    ingredientCard: "[class^=burger-ingredients_burgerIngredient__]",
+    constructorRoot: "[class^=burger-constructor_root__]",
+    ingredientModal: "#ingredientModal",
+    ingredientModalCloseButton: "#ingredientModal button",
+    modalCloseButton: "[class^=modal_closeButton]"
+};
+
 describe("Constructor page works correctly", () => {
     beforeEach(() => {
         cy.intercept("POST", `${API_URL}/orders`).as("postOrder");
@@ -8,48 +16,49 @@ describe("Constructor page works correctly", () => {
         cy.intercept("GET", `${API_URL}/ingredients`).as("getIngredients");
         cy.intercept("POST", `${API_URL}/auth/token`).as("refreshToken");
 
-        cy.visit("http://localhost:3000/login");
+        cy.visit("/login");
         cy.get("[name^=email]").type("akilum@yandex.ru");
         cy.get("[name^=password]").type("1122");
-        cy.get("button").contains("Войти").click();
+        cy.contains("button", "Войти").click();
         cy.wait("@login");
     });
 
     it("should open ingredients popup", () => {
-        // Явно идём на главную
-        cy.visit("http://localhost:3000/");
+        cy.visit("/");
 
         // Ждём, пока загрузятся ингредиенты
         cy.wait("@getIngredients");
 
         // Кликаем по первому ингредиенту
-        cy.get("[class^=burger-ingredients_burgerIngredient__]").first().click();
+        cy.get(SELECTORS.ingredientCard).first().click();
 
         // Проверяем, что модалка открылась
-        cy.get("#ingredientModal").should("be.visible");
+        cy.get(SELECTORS.ingredientModal).should("be.visible");
 
         // Закрываем модалку
-        cy.get("#ingredientModal button").click();
-        cy.get("#ingredientModal").should("not.exist");
+        cy.get(SELECTORS.ingredientModalCloseButton).click();
+        cy.get(SELECTORS.ingredientModal).should("not.exist");
     });
 
     it("constructor should work", () => {
-        cy.visit("http://localhost:3000");
+        cy.visit("/");
         cy.wait("@refreshToken");
-        cy.get("[class^=burger-ingredients_burgerIngredient__]").contains("Мясо").first().as("main");
 
-        cy.get("[class^=burger-ingredients_burgerIngredient__]").contains("булка").first().as("bun");
+        cy.get(SELECTORS.ingredientCard).contains("Мясо").first().as("main");
 
-        cy.get("[class^=burger-constructor_root__]").as("constructor");
-        cy.get("button").contains("Оформить заказ").as("submit");
+        cy.get(SELECTORS.ingredientCard).contains("булка").first().as("bun");
+
+        cy.get(SELECTORS.constructorRoot).as("constructor");
+        cy.contains("button", "Оформить заказ").as("submit");
 
         cy.get("@bun").trigger("dragstart");
         cy.get("@constructor").trigger("drop");
+
         cy.get("@main").trigger("dragstart");
         cy.get("@constructor").trigger("drop");
 
         cy.get("@submit").click().wait("@postOrder");
-        cy.get("[class^=modal_closeButton]").click();
+        cy.get(SELECTORS.modalCloseButton).click();
     });
 });
 
